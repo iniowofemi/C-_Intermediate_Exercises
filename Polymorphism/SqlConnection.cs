34923e5f328980e9b8b1e283c1c4bfe0b3f9a4e7
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2023-02-10 11:02:38 +0000

    Add files via upload

---
+++ Polymorphism/SqlConnection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DatabaseConnection
+{
+    public class SqlConnection : DbConnection
+    {
+        public override void OpenConnection(string ConnectionString)
+        {
+            Console.WriteLine("Connection to SQL Database opened.");
+
+            var validAnswer = false;
+            while (!validAnswer)
+            {
+                Console.WriteLine("Would you like to close the connection? (Y/N)");
+                var choice = Console.ReadLine();
+                if (choice == "N")
+                {
+                    validAnswer = true;
+                    Console.WriteLine("Okay. The Connection will be held open.");
+                }
+                else if (choice == "Y")
+                {
+                    validAnswer = true;
+                    CloseConnection();
+                }
+            }
+        }
+
+        public override void CloseConnection()
+        {
+            Console.WriteLine("Connection to SQL Database closed.");
+        }
+    }
+}
