2ba9c92df5770ce30a346be953f79b27dff66136
iniowofemi <84127526+iniowofemi@users.noreply.github.com>
2023-02-10 11:03:49 +0000

    Add files via upload

---
+++ Polymorphism/Database Connection/DbConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DatabaseConnection
+{
+    public abstract class DbConnection
+    {
+
+        public string ConnectionString { get; set; }
+        public TimeSpan Timeout { get; set; }
+        public abstract void OpenConnection(string ConnectionString);
+
+        public abstract void CloseConnection();     
+    }
+}
